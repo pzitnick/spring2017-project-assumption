ca2fb58588175f46c0c1f06fbac6b5453a548f19
Alexander Chamberlain <alxchamberlain@gmail.com>
2017-04-28 23:32:23 -0700

    Add PathTracer constructor

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -3,6 +3,7 @@ package base;
 import base.data.util.Vector3;
 import base.data.util.Vector4;
 import base.data.util.Vector4Modifier;
+import base.logic.PathTracer;
 
 public class Application {
 
@@ -10,6 +11,8 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        PathTracer pt = new PathTracer();
+
         Vector3 v = new Vector3(1, 2, 3);
         Vector4 v4 = new Vector4(3, 4, 5, 6);
         System.out.println(Vector4Modifier.mul(v4, v));
--- src/main/java/base/logic/PathTracer.java
+++ src/main/java/base/logic/PathTracer.java
@@ -1,7 +1,34 @@
 package base.logic;
 
+import base.data.Scene;
+import base.presentation.Display;
+
 public class PathTracer {
 
+    private Scene scene;
+    private Display display;
+
+    public PathTracer(Scene scene, Display display) {
+        this.scene = scene;
+    }
+
     public PathTracer() {
+        this(new Scene(), new Display());
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public Display getDisplay() {
+        return display;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void setDisplay(Display display) {
+        this.display = display;
     }
 }
