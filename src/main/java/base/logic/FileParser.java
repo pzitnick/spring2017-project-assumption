da4ae2874caf0e4a5f8e227c8fe05c6aa9b2879c
Alexander Chamberlain <alxchamberlain@gmail.com>
2017-05-27 20:28:54 -0700

    Reduce format checks in FileParser.java

--- src/main/java/base/logic/FileParser.java
+++ src/main/java/base/logic/FileParser.java
@@ -15,7 +15,6 @@ public class FileParser {
 
   private static final Logger LOGGER = Logger.getLogger(FileParser.class.getName());
   private static final FileParser INSTANCE = new FileParser();
-  private static final int MIN_TOKENS = 5;
 
   private FileParser() {
   }
@@ -29,72 +28,42 @@ public class FileParser {
 
     List<SceneObject> objects = new ArrayList<>();
 
-    int curLine = 1;
     String line;
-    while ((line = br.readLine()) != null) {
+    while ((line = br.readLine()) != null && line.length() > 0) {
       SceneObject object;
+      // Split line by spaces not inside parentheses
       String[] tokens = line.split("\\s+(?=[^\\])}]*([\\[({]|$))");
 
-      if (tokens.length < MIN_TOKENS) {
-        LOGGER.warning("Illegal SceneObject format on line " + curLine + " (not enough tokens). Skipping...");
-        ++curLine;
-        continue;
-      }
-
       object = getSceneObjectInstance(tokens[0]);
-      if (object == null) {
-        LOGGER.warning("Illegal SceneObject format on line " + curLine +
-            " (no supported SceneObject found). Skipping...");
-        ++curLine;
-        continue;
-      }
 
-      for (int curTok = 1; curTok < MIN_TOKENS; ++curTok) {
-        if (!addSceneObjectParams(object, tokens, curTok)) {
-          LOGGER.warning("Illegal SceneObject format on line " + curLine +
-              " (no supported parameter for SceneObject found). Skipping...");
-          ++curLine;
-          continue;
-        }
+      for (int curTok = 1; curTok < tokens.length; ++curTok) {
+        addSceneObjectParams(object, tokens[curTok]);
       }
 
       objects.add(object);
-      ++curLine;
     }
 
     return objects;
   }
 
-  private boolean addSceneObjectParams(SceneObject sceneObject, String[] tokens, int curTok) {
-    String[] subToken = tokens[curTok].split("\\(");
+  private boolean addSceneObjectParams(SceneObject sceneObject, String token) {
+    String[] subToken = token.split("\\(");
     String[] valueString = subToken[1].split("\\)");
 
     if (valueString.length != 0) {
       String[] values = valueString[0].split(",");
       switch (subToken[0]) {
         case "color":
-          if (values.length != 3) {
-            return false;
-          }
-
           sceneObject.getMaterial().getColor().setRed(Double.parseDouble(values[0].trim()));
           sceneObject.getMaterial().getColor().setGreen(Double.parseDouble(values[1].trim()));
           sceneObject.getMaterial().getColor().setBlue(Double.parseDouble(values[2].trim()));
           break;
         case "emission":
-          if (values.length != 3) {
-            return false;
-          }
-
           sceneObject.getMaterial().getEmission().setRed(Double.parseDouble(values[0].trim()));
           sceneObject.getMaterial().getEmission().setGreen(Double.parseDouble(values[1].trim()));
           sceneObject.getMaterial().getEmission().setBlue(Double.parseDouble(values[2].trim()));
           break;
         case "position":
-          if (values.length != 3) {
-            return false;
-          }
-
           sceneObject.getPosition().setPoint(0, Double.parseDouble(values[0].trim()));
           sceneObject.getPosition().setPoint(1, Double.parseDouble(values[1].trim()));
           sceneObject.getPosition().setPoint(2, Double.parseDouble(values[2].trim()));
@@ -102,8 +71,6 @@ public class FileParser {
         case "radius":
           ((Sphere) sceneObject).setRadius(Double.parseDouble(values[0].trim()));
           break;
-        default:
-          return false;
       }
     }
 
@@ -111,12 +78,10 @@ public class FileParser {
   }
 
   private SceneObject getSceneObjectInstance(String object) {
-    // To be populated with more object types as they are implemented
-    switch (object) {
-      case "sphere":
-        return new Sphere();
-      default:
-        return null;
+    if (object.equals("sphere")) {
+      return new Sphere();
+    } else {
+      return null;
     }
   }
 
--- src/test/java/base/logic/FileParserTest.java
+++ src/test/java/base/logic/FileParserTest.java
@@ -19,32 +19,53 @@ public class FileParserTest extends TestCase {
     try {
       List<SceneObject> objects = FileParser.getInstance().parseSceneObjects(filePath);
 
+      assertEquals(objects.size(), 5);
+
       for (SceneObject object : objects) {
         assertTrue(object instanceof Sphere);
       }
 
       Sphere result0 = (Sphere) objects.get(0);
       Sphere correct0 = new Sphere.SphereBuilder()
-          .material(new Color(0, 0, 0), new Color(0, 0, 0))
-          .position(new Vector3(0, 0, 0))
+          .material(new Color(), new Color())
+          .position(new Vector3())
           .radius(0)
           .build();
       assertEquals(result0, correct0);
 
       Sphere result1 = (Sphere) objects.get(1);
-
+      Sphere correct1 = new Sphere.SphereBuilder()
+          .material(new Color(1, 1, 1), new Color())
+          .position(new Vector3())
+          .radius(0)
+          .build();
+      assertEquals(result1, correct1);
 
       Sphere result2 = (Sphere) objects.get(2);
-
+      Sphere correct2 = new Sphere.SphereBuilder()
+          .material(new Color(1, 1, 1), new Color(2, 2, 2))
+          .position(new Vector3())
+          .radius(0)
+          .build();
+      assertEquals(result2, correct2);
 
       Sphere result3 = (Sphere) objects.get(3);
-
+      Sphere correct3 = new Sphere.SphereBuilder()
+          .material(new Color(1, 1, 1), new Color(2, 2, 2))
+          .position(new Vector3(3, 3, 3))
+          .radius(0)
+          .build();
+      assertEquals(result3, correct3);
 
       Sphere result4 = (Sphere) objects.get(4);
-
+      Sphere correct4 = new Sphere.SphereBuilder()
+          .material(new Color(1, 1, 1), new Color(2, 2, 2))
+          .position(new Vector3(3, 3, 3))
+          .radius(4)
+          .build();
+      assertEquals(result4, correct4);
 
     } catch (IOException e) {
-      fail("Test resource at " + filePath + " not found.");
     }
   }
 
